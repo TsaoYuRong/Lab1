1f6ff5e967c2ba869c0ec2ac206f3bb1154d243a
TsaoYuRong <tammylinca@gmail.com>
2015-03-12 14:12:19 +0800

    cpp

---
+++ lab1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter your number: ";
+	cin>>n;
+	while(n!=1){
+		if(n%2==1)
+      			n=n*3+1;
+   	 	else n=n/2;
+   			cout<<n<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
